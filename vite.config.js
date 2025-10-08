b187a1539afab12bc563f4d333151e556a02fe2e
maptiva <maptiva.sig.app@gmail.com>
2025-10-08 13:13:31 -0300

    feat: Configura GitHub Actions para despliegue con secrets

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tienda-online-mvp/',
+  base: '/tienda-online-mvp/' // ¡MUY IMPORTANTE! Usa el nombre de tu repositorio
 })
