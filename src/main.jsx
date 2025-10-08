faf6ce0441421564a0675a8ad48f2074c5882f30
maptiva <maptiva.sig.app@gmail.com>
2025-10-08 16:56:45 -0300

    fix: Corrige la configuración de despliegue y el enrutamiento

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter  basename="/tienda-online-mvp">
+    <BrowserRouter basename="/tienda-online-mvp">
       <App />
     </BrowserRouter>
   </StrictMode>,
